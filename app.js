12280660ad2edc7ec2261418ecffee386dffd2d9
Manuel Castillo <mancast@gmail.com>
2014-09-01 13:31:39 -0500

    Finished all 3 screens.
    Added styling to the last screen when game is over, added a timer to
    fade the game out, corrected some issues.

--- app.js
+++ app.js
@@ -3,40 +3,56 @@ $(document).ready(function() {
 	var computerNumber1;
 	var computerNumber2;
 	var userNumber;
+	var newGame;
+	var total;
+	var timeOut;
+	var count = 0
 
 
-	
+	$("#count").text("You have answered " + count + " correct!");
+
+	function newGame() {
+		newNumbers();
+		setTimeout(function() {
+				$(".game").fadeOut();
+				$(".timeUp").fadeIn();
+				$("#count").text("You have answered " + count + " correct!");
+				console.log("You have answered " + count + " correct!");
+			}, 60000);
+
+	}
+
+
+	function newNumbers() {
 		computerNumber1 = Math.floor(Math.random() * 101);
 		console.log(computerNumber1);
-
 		computerNumber2 = Math.floor(Math.random() * 101);
 		console.log(computerNumber2);
-
 		$("#problem1").append(computerNumber1);
 		$("#problem2").append(computerNumber2);
-		var total = computerNumber1 + computerNumber2;
+		total = computerNumber1 + computerNumber2;
 		console.log(total);
 		$("#userGuess").val(""); 
-
- 
+	}
 
 	
-
+ 
 	$("#userGuess").keypress(function (e) { 
 	    if (e.keyCode == 13) { 
-	    	userNumber = $("#userGuess").val();
-	    	if (userNumber == total) {
-	    			location.reload();
-	    			$("#userGuess").val("");
-	    			event.preventDefault();
-	   		} else {
-	   			$("#message").text("Try Again...");
-	   			$("#userGuess").val(""); 
-	   		}
+		    	userNumber = $("#userGuess").val();
+		    	if (userNumber == total) {
+		    			$("#userGuess").val("");
+		    			$("#problem1, #problem2").empty();
+		    			newNumbers();
+		    			count++;
+		    			console.log(count);
+		   		} else {
+		   			$("#message").text("Try Again...");
+		   			$("#userGuess").val(""); 
+	   			}
 	    }
-
-
 	});
+
 
 	$("#goBack").click(function() {
 		$(".game").fadeOut();
@@ -47,7 +63,22 @@ $(document).ready(function() {
 	$("#play").click(function() {
 		$(".instructions").fadeOut();
 		$(".game").fadeIn();
-	})
+		newGame();
+	});
+
+	$("#yes").click(function() {
+		$(".timeUp").fadeOut();
+		$(".game").fadeIn();
+		$("#problem1, #problem2").empty();
+		newGame();
+	});
+
+	$("#no").click(function() {
+		$(".timeUp").fadeOut();
+		$(".instructions").fadeIn();
+		$("#problem1, #problem2").empty();
+	});
+
 
 	 
 
